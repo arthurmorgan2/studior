651b7b7e00dc21a9214c82334d0eabf0ecf1ca35
arthurmorgan2 <nurakura9@gmail.com>
2024-05-13 10:46:42 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enable static generation
-    target: 'static',
-    // Output directory for static HTML files
+    static: true,
     output: 'public',
-    // Enable internationalization support
-    i18n: false,
-    // Enable server-side rendering (optional)
-    // target: 'serverless',
 };
 
 export default nextConfig;
