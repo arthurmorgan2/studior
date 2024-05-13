5497a8e66b1485834efc50e333e947272a993dd4
arthurmorgan2 <nurakura9@gmail.com>
2024-05-13 12:12:31 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    static: true,
-    path: 'public',
-    output: 'export',
-    images: { unoptimized: true }
+    // static: true,
+    // path: 'public',
+    // output: 'export',
+    // images: { unoptimized: true }
 };
 
 export default nextConfig;
